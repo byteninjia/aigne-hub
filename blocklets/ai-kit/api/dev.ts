33214d1a893599462135bf4949491ecc9b77c5a8
Yechao Li <liyechao@yechao.xyz>
2024-04-20 23:30:37 +0800

    chore: public access hmr

--- blocklets/ai-kit/api/dev.ts
+++ blocklets/ai-kit/api/dev.ts
@@ -3,4 +3,7 @@ import { setupClient } from 'vite-plugin-blocklet';
 
 import { app } from './src';
 
-setupClient(app);
+const hmrPort = process.env.__HMR_PORT__;
+const hmrProtocol = process.env.__HMR_HOSTNAME__ ? 'wss' : undefined;
+
+setupClient(app, hmrPort ? { port: parseInt(hmrPort, 10), protocol: hmrProtocol } : undefined);
--- blocklets/ai-kit/vite.config.ts
+++ blocklets/ai-kit/vite.config.ts
@@ -6,10 +6,38 @@ import { createBlockletPlugin } from 'vite-plugin-blocklet';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+const hmrHostName = process.env.__HMR_HOSTNAME__;
+const hmrPath = process.env.__HMR_PATH__;
+
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   return {
-    plugins: [tsconfigPaths(), react(), createBlockletPlugin({ disableNodePolyfills: false }), svgr()],
+    plugins: [
+      tsconfigPaths(),
+      react(),
+      createBlockletPlugin({ disableNodePolyfills: false }),
+      svgr(),
+      hmrHostName
+        ? {
+            name: 'client-host',
+            transform(code, id) {
+              if (id.endsWith('dist/client/client.mjs') || id.endsWith('dist/client/env.mjs')) {
+                return code.replace('__HMR_HOSTNAME__', JSON.stringify(hmrHostName));
+              }
+
+              return code;
+            },
+          }
+        : undefined,
+    ],
+    server: {
+      hmr: hmrPath
+        ? {
+            clientPort: 443,
+            path: hmrPath,
+          }
+        : undefined,
+    },
     build: {
       commonjsOptions: {
         transformMixedEsModules: true,
