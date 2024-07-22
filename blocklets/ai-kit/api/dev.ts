6a5c56f35c19306b0ba95f995d51e598b4daf25f
Yechao Li <liyechao@yechao.xyz>
2024-07-22 09:11:45 +0800

    chore: use vite-node to improve development experience

--- blocklets/ai-kit/api/dev.ts
+++ blocklets/ai-kit/api/dev.ts
@@ -1,9 +1,9 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { setupClient } from 'vite-plugin-blocklet';
 
-import { app } from './src';
+import { app, server } from './src';
 
-const hmrPort = process.env.__HMR_PORT__;
-const hmrProtocol = process.env.__HMR_HOSTNAME__ ? 'wss' : undefined;
-
-setupClient(app, hmrPort ? { port: parseInt(hmrPort, 10), protocol: hmrProtocol } : undefined);
+setupClient(app, {
+  server,
+  importMetaHot: import.meta.hot,
+});
