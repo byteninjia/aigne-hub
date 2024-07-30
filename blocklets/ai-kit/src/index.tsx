43865e5f1408b1f7842aa42e3f65f053af83db9f
Yechao Li <liyechao@yechao.xyz>
2024-07-30 16:25:39 +0800

    chore: update deps

--- blocklets/ai-kit/src/index.tsx
+++ blocklets/ai-kit/src/index.tsx
@@ -2,5 +2,13 @@ import ReactDOM from 'react-dom/client';
 
 import App from './app';
 
+const SUPPRESSED_WARNINGS = ['Support for defaultProps will be removed'];
+const { error } = console;
+console.error = (msg, ...args: any[]) => {
+  if (typeof msg === 'string' && !SUPPRESSED_WARNINGS.some((entry) => msg.includes(entry))) {
+    error(msg, ...args);
+  }
+};
+
 const root = ReactDOM.createRoot(document.getElementById('app')!);
 root.render(<App />);
